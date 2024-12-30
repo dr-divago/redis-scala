376e3f0e0e40bab6c4b1d04668dfffe20430a96d
dr-divago <matteo.pelliccia@gmail.com>
2024-12-30 21:24:19 +0100

    Third try

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,20 +1,21 @@
 package codecrafters_redis
 
+import java.io.{BufferedReader, InputStreamReader}
 import java.net.{InetSocketAddress, ServerSocket}
 
 object Server {
   def main(args: Array[String]): Unit = {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
 
-    // Uncomment this to pass the first stage
-    //
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
     val clientSocket = serverSocket.accept()
 
-    val out = clientSocket.getOutputStream
-    while (!serverSocket.isClosed)
+    val in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream))
+
+    while (in.readLine() != null) {
+      val out = clientSocket.getOutputStream
       out.write("+PONG\r\n".getBytes)
+    }
   }
 }
