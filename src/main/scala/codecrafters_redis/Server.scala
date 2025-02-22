af28f0850e01dcac9377393a58da3e24252a8a3d
dr-divago <matteo.pelliccia@gmail.com>
2025-02-22 18:31:14 +0100

    codecrafters submit [skip ci]

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -3,8 +3,9 @@ package codecrafters_redis
 import codecrafters_redis.config.{Config, Context}
 import codecrafters_redis.eventloop.EventLoop
 
-import java.io.PrintWriter
+import java.io.{BufferedReader, InputStreamReader, PrintWriter}
 import java.net.Socket
+import java.nio.ByteBuffer
 
 object Server {
   def main(args: Array[String]): Unit = {
@@ -22,12 +23,35 @@ object Server {
 
     val master_ip_port = config.replicaof.split(" ")
     val master_socket = new Socket("localhost", master_ip_port(1).toInt)
+    val port = config.port
     val out = new PrintWriter(master_socket.getOutputStream)
+    val in = master_socket.getInputStream
+
+    val buffer = ByteBuffer.allocate(17)
 
     out.write("*1\r\n$4\r\nPING\r\n")
     out.flush()
-    out.close()
-    master_socket.close()
+    in.read(buffer.array(), 0, 7)
+    val pingResponse = new String(buffer.array(), 0, 7)
+    if (!pingResponse.equals("+PONG\r\n")) {
+      throw new Exception(s"Expected +PONG but got ${pingResponse} ${pingResponse.length}")
+    }
+
+    out.write(s"*3\r\n$$8\r\nREPLCONF\r\n$$14\r\nlistening-port\r\n$$${port.length}\r\n$port\r\n")
+    out.flush()
+    in.read(buffer.array(), 7, 5)
+    val ok1Response = new String(buffer.array(), 7, 5)
+    if (ok1Response != "+OK\r\n") {
+      throw new Exception(s"Expected +OK but got ${ok1Response} ")
+    }
+
+    out.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
+    out.flush()
+    in.read(buffer.array(), 12, 5)
+    val ok2Response = new String(buffer.array(), 12, 5)
+    if (ok2Response != "+OK\r\n") {
+      throw new Exception(s"Expected +OK but go ${ok2Response}")
+    }
     println("Sent PING to master")
   }
 }
