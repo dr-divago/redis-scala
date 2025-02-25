97741ec44d16b8759ad1d6df6224d80c9def770b
dr-divago <matteo.pelliccia@gmail.com>
2025-02-25 08:48:45 +0100

    Refactor EventLoop

--- src/main/scala/codecrafters_redis/eventloop/EventLoop.scala
+++ src/main/scala/codecrafters_redis/eventloop/EventLoop.scala
@@ -47,17 +47,35 @@ class EventLoop(context: Context) {
 
 
   private def setupReplication(config: Config): Unit = {
-    val master_ip_port = config.replicaof.split(" ")
-    val master_socket = new Socket("localhost", master_ip_port(1).toInt)
+    val masterIpPort = config.replicaof.split(" ")
+    val masterChannel = SocketChannel.open()
+    masterChannel.configureBlocking(false)
+
+    val connected = masterChannel.connect(new InetSocketAddress(masterIpPort(0), masterIpPort(1).toInt))
+
+    if (connected) {
+      println("connected to server")
+    }
+    else {
+      println("Not connected yet")
+    }
+
+    /*
+    val masterSocket = new Socket(masterIpPort(0), masterIpPort(1).toInt)
     val port = config.port
-    val out = new PrintWriter(master_socket.getOutputStream)
-    val in = master_socket.getInputStream
+    val out = new PrintWriter(masterSocket.getOutputStream)
+    val in = masterSocket.getInputStream
 
     val buffer = ByteBuffer.allocate(17)
 
     out.write("*1\r\n$4\r\nPING\r\n")
     out.flush()
-    in.read(buffer.array(), 0, 7)
+    var bytesRead = 0
+    while (bytesRead < 7) {
+      val count = in.read(buffer.array(), 0, 7)
+      if (count == -1) throw new IOException("Connection closed")
+      bytesRead += count
+    }
     val pingResponse = new String(buffer.array(), 0, 7)
     if (!pingResponse.equals("+PONG\r\n")) {
       throw new Exception(s"Expected +PONG but got ${pingResponse} ${pingResponse.length}")
@@ -93,7 +111,7 @@ class EventLoop(context: Context) {
     }
     println("REPLICA READ FULLRESYNC")
 
-    /*
+
     // Read RDB file
     // First read the RDB file size
     val rdbSizeBuffer = new StringBuilder()
@@ -114,17 +132,21 @@ class EventLoop(context: Context) {
     }
 
 
-     */
+
 
     println("Handshake completed successfully, RDB file received")
-    /*
+
     in.read(buffer.array(), 12, 5)
     val fullResyncResponse = new String(buffer.array(), 12, 5)
     if (ok2Response != "+OK\r\n") {
       throw new Exception(s"Expected +OK but go ${ok2Response}")
     }
 
+
+
      */
+
+
 
   }
 
