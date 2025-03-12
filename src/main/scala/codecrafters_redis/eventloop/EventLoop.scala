85ed144d7a8a8ce963bc3141365e5ea07c9a7125
dr-divago <matteo.pelliccia@gmail.com>
2025-03-12 21:00:10 +0100

    Refactor EventLoop

--- src/main/scala/codecrafters_redis/eventloop/EventLoop.scala
+++ src/main/scala/codecrafters_redis/eventloop/EventLoop.scala
@@ -45,16 +45,11 @@ class EventLoop(context: Context) {
               replicationState = readData(key, replicationState)
             case key if key.isConnectable =>
               replicationState = connectClient(key, replicationState)
-            case key if key.isWritable => writeData(key)
           }
           iterator.remove()
         }
       }
     }
-  }
-
-  private def writeData(key: SelectionKey) = {
-    println("Write data")
   }
 
   private def connectClient(key: SelectionKey, replicationState: Option[ReplicationState]): Option[ReplicationState] = {
@@ -106,95 +101,6 @@ class EventLoop(context: Context) {
       masterChannel.register(selector, SelectionKey.OP_CONNECT)
       initialState
     }
-
-    /*
-    val masterSocket = new Socket(masterIpPort(0), masterIpPort(1).toInt)
-    val port = config.port
-    val out = new PrintWriter(masterSocket.getOutputStream)
-    val in = masterSocket.getInputStream
-
-    val buffer = ByteBuffer.allocate(17)
-
-    out.write("*1\r\n$4\r\nPING\r\n")
-    out.flush()
-    var bytesRead = 0
-    while (bytesRead < 7) {
-      val count = in.read(buffer.array(), 0, 7)
-      if (count == -1) throw new IOException("Connection closed")
-      bytesRead += count
-    }
-    val pingResponse = new String(buffer.array(), 0, 7)
-    if (!pingResponse.equals("+PONG\r\n")) {
-      throw new Exception(s"Expected +PONG but got ${pingResponse} ${pingResponse.length}")
-    }
-
-    out.write(s"*3\r\n$$8\r\nREPLCONF\r\n$$14\r\nlistening-port\r\n$$${port.length}\r\n$port\r\n")
-    out.flush()
-    in.read(buffer.array(), 7, 5)
-    val ok1Response = new String(buffer.array(), 7, 5)
-    if (ok1Response != "+OK\r\n") {
-      throw new Exception(s"Expected +OK but got ${ok1Response} ")
-    }
-
-    out.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")
-    out.flush()
-    in.read(buffer.array(), 12, 5)
-    val ok2Response = new String(buffer.array(), 12, 5)
-    if (ok2Response != "+OK\r\n") {
-      throw new Exception(s"Expected +OK but go ${ok2Response}")
-    }
-
-    out.write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n")
-    out.flush()
-
-    // Read FULLRESYNC response
-    println("REPLICA INIT BUFFER")
-    val fullResyncBuffer = ByteBuffer.allocate(1024)
-    val fullResyncBytes = in.read(fullResyncBuffer.array())
-    val fullResyncResponse = new String(fullResyncBuffer.array(), 0, fullResyncBytes)
-
-    if (!fullResyncResponse.startsWith("+FULLRESYNC")) {
-      throw new Exception(s"Expected FULLRESYNC but got ${fullResyncResponse}")
-    }
-    println("REPLICA READ FULLRESYNC")
-
-
-    // Read RDB file
-    // First read the RDB file size
-    val rdbSizeBuffer = new StringBuilder()
-    var char = in.read().toChar
-    while (char != '\r') {
-      rdbSizeBuffer.append(char)
-      char = in.read().toChar
-    }
-    in.read() // consume \n
-
-    val rdbSize = rdbSizeBuffer.toString().substring(1).toInt // Remove $ from size
-    val rdbBuffer = ByteBuffer.allocate(rdbSize)
-    var totalRead = 0
-    while (totalRead < rdbSize) {
-      val read = in.read(rdbBuffer.array(), totalRead, rdbSize - totalRead)
-      if (read == -1) throw new Exception("Unexpected end of RDB file")
-      totalRead += read
-    }
-
-
-
-
-    println("Handshake completed successfully, RDB file received")
-
-    in.read(buffer.array(), 12, 5)
-    val fullResyncResponse = new String(buffer.array(), 12, 5)
-    if (ok2Response != "+OK\r\n") {
-      throw new Exception(s"Expected +OK but go ${ok2Response}")
-    }
-
-
-
-     */
-
-
-
   }
 
   private def acceptClient(key: SelectionKey): Unit = {
